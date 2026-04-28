76d37951b2d0a0bed61890978f435e79e26a3914
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-28 13:05:19 +0530

    feat: 404 super class initiate

--- src/main/java/lk/job_finder_app/smart_job_aggregator/globalExceptionHandler/ResourceNotFoundException.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/globalExceptionHandler/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package lk.job_finder_app.smart_job_aggregator.globalExceptionHandler;
 
 public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
