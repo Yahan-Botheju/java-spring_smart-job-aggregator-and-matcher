7d9d8ada15f3468233d64c5369ef4c0d0c49961e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-28 13:03:41 +0530

    feat: 400 super class initiate

---
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/globalExceptionHandler/ConflictException.java
@@ -0,0 +1,7 @@
+package lk.job_finder_app.smart_job_aggregator.globalExceptionHandler;
+
+public class ConflectException extends RuntimeException {
+    public ConflectException(String message) {
+        super(message);
+    }
+}
