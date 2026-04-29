af95f4309e10bd8e1f23b17b6d277b8b68e83075
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 13:51:35 +0530

    feat: create and initiate job applicatioin entity fields and realtionship between other tables

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/persistence/entity/JobApplicationEntity.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/persistence/entity/JobApplicationEntity.java
@@ -1,4 +1,42 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lk.job_finder_app.smart_job_aggregator.domain.models.enums.ApplicationStatus;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.entity.JobPostEntity;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "jobApplications")
+@SoftDelete(columnName = "is_deleted")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobApplicationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long jobApplicationId;
+    @NotBlank(message = "URL cannot be empty")
+    private String resumeUrl;
+    @NotNull(message = "Matching Score cannot be empty")
+    private double matchScore;
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus applicationStatus;
+    @NotNull(message = "Date time cannot be empty")
+    private LocalDateTime appliedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "postId",  nullable = false)
+    private JobPostEntity jobPost;
 }
