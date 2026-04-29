0f36ef7d5aa7700c2631ee93b60f1adc46e7ce67
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 20:02:13 +0530

    fix: remove soft delete in role entity

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/role/persistence/entity/RoleEntity.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/role/persistence/entity/RoleEntity.java
@@ -3,16 +3,13 @@ package lk.job_finder_app.smart_job_aggregator.infrastructure.role.persistence.e
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lk.job_finder_app.smart_job_aggregator.domain.models.enums.RoleName;
-import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SoftDelete;
 
 
 @Entity
 @Table(name = "roles")
-@SoftDelete(columnName = "is_deleted")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
