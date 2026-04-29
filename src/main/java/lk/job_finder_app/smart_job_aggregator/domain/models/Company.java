adf5b686f865a111248bfb9306322fbe98f6f67f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 13:47:29 +0530

    feat: create and initiate company domain model fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/Company.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/Company.java
@@ -1,4 +1,15 @@
 package lk.job_finder_app.smart_job_aggregator.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Company {
+    private Long companyId;
+    private String companyName;
+    private String companyIndustry;
+    private String companyRating;
 }
