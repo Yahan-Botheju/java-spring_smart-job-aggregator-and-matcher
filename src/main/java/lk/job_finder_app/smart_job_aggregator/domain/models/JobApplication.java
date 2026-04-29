e26ea7d4b940edb75d2e59cf09cc7610bbf02227
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 10:22:31 +0530

    feat: initiate job application domain fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/JobApplication.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/JobApplication.java
@@ -1,4 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobApplication {
+    private Long jobApplicationId;
+    private Long userId;
+    private Long jobPostId;
+    private String resumeUrl;
+    private double matchScore;
+    private LocalDateTime appliedAt;
 }
