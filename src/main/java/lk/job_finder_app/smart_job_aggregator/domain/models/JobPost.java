0256d22dc8c917be1e84b77f217226b81788ec0d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 10:14:54 +0530

    feat: initiate job post domain model fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/JobPost.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/JobPost.java
@@ -1,4 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobPost {
+    private Long postId;
+    private String title;
+    private String description;
+    private BigDecimal salary;
+    private Set<String> skillsRequired;
 }
