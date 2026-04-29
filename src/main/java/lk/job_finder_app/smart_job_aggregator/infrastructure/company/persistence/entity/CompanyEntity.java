75e602d1b6b6fe197a885941c8d0c47e97fd0d0c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 13:50:58 +0530

    feat: create and initiate company entity fields and realtionship between other tables

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/company/persistence/entity/CompanyEntity.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/company/persistence/entity/CompanyEntity.java
@@ -1,4 +1,30 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.company.persistence.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "companies")
+@SoftDelete(columnName = "is_deleted")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long companyId;
+    @NotBlank(message = "company name cannot be empty")
+    private String companyName;
+    @NotBlank(message = "Company industry cannot be empty")
+    private String companyIndustry;
+    @NotNull(message = "Rating cannot be empty")
+    private BigDecimal companyRating;
 }
