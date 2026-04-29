9d30155a7128fabafe884bf8c3f4b404508f9dc7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 13:49:10 +0530

    feat: create and initiate user domain model fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/User.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/User.java
@@ -1,4 +1,14 @@
 package lk.job_finder_app.smart_job_aggregator.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private Long userId;
+    private String userName;
+    private String userEmail;
 }
