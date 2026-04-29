10d4c07e6e453191c3488db3412bcec7d3bc737a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-29 13:48:48 +0530

    feat: create and initiate role domain model fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/Role.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/Role.java
@@ -1,4 +1,15 @@
 package lk.job_finder_app.smart_job_aggregator.domain.models;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.enums.RoleName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
+    private Long roleId;
+    private RoleName roleName;
+    private String limitPerMinute;
 }
