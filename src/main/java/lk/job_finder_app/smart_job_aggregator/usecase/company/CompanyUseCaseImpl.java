e5c2d1e1aa20ae87258075b1b73d2097462654fa
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 10:08:29 +0530

    feat: inject company repo

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCaseImpl.java
@@ -1,4 +1,11 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.company;
 
-public class CompanyUseCaseImpl {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CompanyUseCaseImpl implements CompanyUseCase {
+
+    //inject company repository
+    private final CompanyRepository companyRepository;
 }
