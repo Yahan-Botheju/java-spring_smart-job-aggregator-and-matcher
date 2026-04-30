c6af5c8e6ad40dcc20120f68b9f00b698bc8910c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 10:35:44 +0530

    feat: config job application usecase impl bean

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/config/JobApplicationUseCaseBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/config/JobApplicationUseCaseBeanConfig.java
@@ -1,4 +1,17 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.config;
 
-public class JobApplicationUseCaseBeanConfig {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.usecase.jobApplication.JobApplicationUseCase;
+import lk.job_finder_app.smart_job_aggregator.usecase.jobApplication.JobApplicationUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class JobApplicationUseCaseBeanConfig {
+    @Bean
+    JobApplicationUseCase jobApplicationUseCase(
+            JobApplicationRepository jobApplicationRepository
+    ) {
+        return new JobApplicationUseCaseImpl(jobApplicationRepository);
+    }
 }
--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobApplication/JobApplicationUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobApplication/JobApplicationUseCaseImpl.java
@@ -1,4 +1,11 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.jobApplication;
 
-public class JobApplicationUseCaseImpl {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JobApplicationUseCaseImpl implements JobApplicationUseCase {
+
+    //inject job application repo
+    private final JobApplicationRepository jobApplicationRepository;
 }
