c8a86c518615d2ea6b5889dfc31af192d960a5ec
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 11:19:06 +0530

    feat: config user repository bean

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/config/UserPersistenceBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/config/UserPersistenceBeanConfig.java
@@ -1,4 +1,20 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.user.config;
 
-public class UserPersistenceBeanConfig {
+import lk.job_finder_app.smart_job_aggregator.domain.models.User;
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.UserRepositoryImpl;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.jpa.JpaUserRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.mapper.UserPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class UserPersistenceBeanConfig {
+    @Bean
+    UserRepository userRepository(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper
+    ) {
+        return new UserRepositoryImpl(jpaUserRepository, userPersistenceMapper);
+    }
 }
