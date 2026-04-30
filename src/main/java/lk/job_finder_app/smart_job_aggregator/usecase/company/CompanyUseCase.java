714ef44d5f132e4ac897c09864384a0a80514de9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 20:42:48 +0530

    feat: initiate get all companies method in interfaces

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCase.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCase.java
@@ -1,4 +1,11 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.company;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.Company;
+
+import java.util.List;
+
 public interface CompanyUseCase {
+
+    //get all companies
+    List<Company> getAllCompanies();
 }
