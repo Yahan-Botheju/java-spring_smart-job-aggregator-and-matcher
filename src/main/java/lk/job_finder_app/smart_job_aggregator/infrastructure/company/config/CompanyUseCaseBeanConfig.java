a151dac209501f41a4279858cfeddae6481ae6f1
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 10:07:43 +0530

    feat: config company use case bean

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/company/config/CompanyUseCaseBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/company/config/CompanyUseCaseBeanConfig.java
@@ -1,4 +1,15 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.company.config;
 
-public class CompanyUseCaseBeanConfig {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
+import lk.job_finder_app.smart_job_aggregator.usecase.company.CompanyUseCase;
+import lk.job_finder_app.smart_job_aggregator.usecase.company.CompanyUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class CompanyUseCaseBeanConfig {
+    @Bean
+    CompanyUseCase companyUseCase(CompanyRepository companyRepository) {
+        return new CompanyUseCaseImpl(companyRepository);
+    }
 }
