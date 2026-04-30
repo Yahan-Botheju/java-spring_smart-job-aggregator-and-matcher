140383f10933bb02d447c4790fb2f64405c6a814
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 12:38:37 +0530

    feat: initiate req and res DTOs templates fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/company/DTOs/CompanyRequestDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/company/DTOs/CompanyRequestDTO.java
@@ -1,4 +1,21 @@
 package lk.job_finder_app.smart_job_aggregator.web.company.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyRequestDTO {
+    @NotBlank(message = "Company Name cannot be empty")
+    private String companyName;
+    @NotBlank(message = "Company Industry cannot be empty")
+    private String companyIndustry;
+    @NotNull(message = "Company rating cannot be empty" )
+    private BigDecimal companyRating;
 }
