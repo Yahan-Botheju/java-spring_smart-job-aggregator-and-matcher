b09971202c4fbafed0dc204e34e386e6c0ebc6f6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 20:45:52 +0530

    feat: get value through domian repo

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCaseImpl.java
@@ -1,11 +1,20 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.company;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.Company;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CompanyUseCaseImpl implements CompanyUseCase {
 
     //inject company repository
     private final CompanyRepository companyRepository;
+
+    //get all companies
+    @Override
+    public List<Company> getAllCompanies(){
+        return  companyRepository.getAllCompanies();
+    }
 }
