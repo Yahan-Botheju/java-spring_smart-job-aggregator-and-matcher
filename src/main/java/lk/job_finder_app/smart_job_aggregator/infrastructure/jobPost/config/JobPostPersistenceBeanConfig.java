37c88ebbc31b5005d97ffe604656ba376588fed5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 11:10:06 +0530

    feat: config job post  persistence bean

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/config/JobPostPersistenceBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/config/JobPostPersistenceBeanConfig.java
@@ -1,4 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.config;
 
-public class JobPostPersistenceBeanConfig {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.JobPostRepositoryImpl;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.jpa.JpaJobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.mapper.JobPostPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class JobPostPersistenceBeanConfig {
+    @Bean
+    JobPostRepository jobPostRepository(
+            JpaJobPostRepository jobPostRepository,
+            JobPostPersistenceMapper jobPostPersistenceMapper
+    ) {
+        return new JobPostRepositoryImpl(jobPostRepository, jobPostPersistenceMapper);
+    }
 }
--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/persistence/JobPostRepositoryImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/persistence/JobPostRepositoryImpl.java
@@ -1,4 +1,16 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence;
 
-public class JobPostRepositoryImpl {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.jpa.JpaJobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.mapper.JobPostPersistenceMapper;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JobPostRepositoryImpl implements JobPostRepository {
+
+    //inject jpa repo
+    private final JpaJobPostRepository jpaJobPostRepository;
+
+    //inject persistence mapper
+    private final JobPostPersistenceMapper jobPostPersistenceMapper;
 }
