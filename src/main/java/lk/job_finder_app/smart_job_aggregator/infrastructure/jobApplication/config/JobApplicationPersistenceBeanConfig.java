31891a24e86d5518f48d11b5784958a3e617ee9d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 10:46:01 +0530

    feat: config job application persistence bean

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/config/JobApplicationPersistenceBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/config/JobApplicationPersistenceBeanConfig.java
@@ -1,4 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.config;
 
-public class JobApplicationPersistenceBeanConfig {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.JobApplicationRepositoryImpl;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.jpa.JpaJobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.mapper.JobApplicationPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class JobApplicationPersistenceBeanConfig implements JobApplicationRepository {
+    @Bean
+    JobApplicationRepository jobApplicationRepository(
+            JpaJobApplicationRepository jpaJobApplicationRepository,
+            JobApplicationPersistenceMapper jobApplicationPersistenceMapper
+    ) {
+        return new JobApplicationRepositoryImpl(jpaJobApplicationRepository, jobApplicationPersistenceMapper);
+    }
 }
