8719fa48c7bc11dfc32c1689eac2bd807e40ceb2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-30 11:15:35 +0530

    feat: confgi user usecase bean

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/user/UserUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/user/UserUseCaseImpl.java
@@ -1,4 +1,11 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.user;
 
-public class UserUseCaseImpl {
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserUseCaseImpl implements UserUseCase{
+
+    //inject user repo
+    private final UserRepository userRepository;
 }
