33dd36a230661020a7df89062c6686810ce3b3eb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-01 19:41:59 +0530

    feat: initiate job post request dto fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/DTOs/JobPostRequestDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/DTOs/JobPostRequestDTO.java
@@ -1,4 +1,25 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobPost.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobPostRequestDTO {
+    @NotBlank(message = "post title cannot be empty")
+    private String postTitle;
+    @NotBlank(message = "post description cannot be empty")
+    private String postDescription;
+    @NotNull(message = "post salary cannot be empty")
+    private BigDecimal postSalary;
+    @NotEmpty(message = "at least one skill is required")
+    private Set<String> skillsRequired;
 }
