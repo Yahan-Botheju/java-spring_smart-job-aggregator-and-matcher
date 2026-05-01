78d8c5a4b4a9b8a200146554609d7e729adc58d5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-01 12:22:19 +0530

    feat: chcek values and set into domain repo

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/company/CompanyUseCaseImpl.java
@@ -3,6 +3,7 @@ package lk.job_finder_app.smart_job_aggregator.usecase.company;
 import lk.job_finder_app.smart_job_aggregator.domain.models.Company;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.List;
 
@@ -23,5 +24,15 @@ public class CompanyUseCaseImpl implements CompanyUseCase {
     @Override
     public Company createCompany(Company company){
         return companyRepository.createCompany(company);
+    }
+
+    //update company
+    @Override
+    public Company updateCompany(Long companyId, Company company){
+        //check company availability by custom method
+        companyRepository.getCompanyById(companyId)
+                .orElseThrow(() -> new ResourceNotFoundException("Company not found" + companyId));
+        //set values to domain repo
+        return companyRepository.updateCompany(companyId, company);
     }
 }
