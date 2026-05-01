437fe32c913895f5b37d5725eb3cc08f0593ea59
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-01 19:42:28 +0530

    feat: initiate job post response dto fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/DTOs/JobPostResponseDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/DTOs/JobPostResponseDTO.java
@@ -1,4 +1,22 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobPost.DTOs;
 
+
+import lk.job_finder_app.smart_job_aggregator.domain.models.enums.JobStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobPostResponseDTO {
+    private Long postId;
+    private String postTitle;
+    private String postDescription;
+    private BigDecimal postSalary;
+    private JobStatus jobStatus;
+    private Set<String> skillsRequired;
 }
