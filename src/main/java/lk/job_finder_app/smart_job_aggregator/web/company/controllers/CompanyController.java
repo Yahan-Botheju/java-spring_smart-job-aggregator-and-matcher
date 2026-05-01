7e1ecd1d9d1f2426fa1cd235acb217b539c66dcf
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-01 11:38:08 +0530

    feat: setup create company route

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/company/controllers/CompanyController.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/company/controllers/CompanyController.java
@@ -3,14 +3,14 @@ package lk.job_finder_app.smart_job_aggregator.web.company.controllers;
 import lk.job_finder_app.smart_job_aggregator.domain.models.Company;
 import lk.job_finder_app.smart_job_aggregator.globalResponseHandler.StandardResponse;
 import lk.job_finder_app.smart_job_aggregator.usecase.company.CompanyUseCase;
+import lk.job_finder_app.smart_job_aggregator.web.company.DTOs.CompanyRequestDTO;
 import lk.job_finder_app.smart_job_aggregator.web.company.DTOs.CompanyResponseDTO;
 import lk.job_finder_app.smart_job_aggregator.web.company.webMappers.CompanyWebMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -39,6 +39,26 @@ public class CompanyController {
                 responseDTOS
         ));
 
+    }
+
+    //create company
+    @PostMapping("/")
+    public ResponseEntity<StandardResponse<CompanyResponseDTO>> createCompany(
+                               @RequestBody CompanyRequestDTO companyRequestDTO
+    ){
+        //turn dot to domain model
+        Company company = companyWebMapper.toDomainModel(companyRequestDTO);
+
+        //set to usecase
+        Company savedEntity = companyUseCase.createCompany(company);
+
+        return ResponseEntity.created(URI.create("/api/v1/jobsapplicator/company/"))
+                .body(new StandardResponse<>(
+                        201,
+                        "Company registered successfully",
+                        LocalDateTime.now(),
+                        companyWebMapper.toResponseDTO(savedEntity)
+                ));
     }
 
 }
