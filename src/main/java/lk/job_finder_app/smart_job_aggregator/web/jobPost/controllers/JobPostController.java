3a842bb7ad0c65b959562f897a180df5a70cc541
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-02 19:11:55 +0530

    feat: setup create new job post route

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/controllers/JobPostController.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/controllers/JobPostController.java
@@ -1,6 +1,7 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobPost.controllers;
 
 import lk.job_finder_app.smart_job_aggregator.domain.models.JobPost;
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobPostWithCompany;
 import lk.job_finder_app.smart_job_aggregator.globalResponseHandler.StandardResponse;
 import lk.job_finder_app.smart_job_aggregator.usecase.jobPost.JobPostUseCase;
 import lk.job_finder_app.smart_job_aggregator.web.jobPost.DTOs.JobPostRequestDTO;
@@ -41,19 +42,24 @@ public class JobPostController {
         ));
     }
 
+    //create new post
     @PostMapping("/")
     public ResponseEntity<StandardResponse<JobPostResponseDTO>> createJobPost(
             @RequestBody JobPostRequestDTO jobPostRequestDTO
             ){
-        //turn to domain model and set to usecase
-        JobPost jobPostDomainModel = jobPostUseCase.createJobPost(jobPostWebMapper.toDomainModel(jobPostRequestDTO));
+        //turn requestDTO to domain model
+        JobPost toDomainModel = jobPostWebMapper.toDomainModel(jobPostRequestDTO);
+        //set values to usecase (custom job post and company method)
+        JobPostWithCompany savedJobPost = jobPostUseCase.createJobPost(toDomainModel);
+        //get response with company name id using custom mapper
+        JobPostResponseDTO responseDTO = jobPostWebMapper.customerResponseDTO(savedJobPost);
 
         return ResponseEntity.created(URI.create("/api/v1/jobsapplicator/jobpost/"))
                 .body(new StandardResponse<>(
                         201,
-                        "Job Post created successfully",
+                        "Job Post created successfully" + " , " +  responseDTO.getPostId(),
                         LocalDateTime.now(),
-                        jobPostWebMapper.toResponseDTO(jobPostDomainModel)
+                        responseDTO
                 ));
 
     }
