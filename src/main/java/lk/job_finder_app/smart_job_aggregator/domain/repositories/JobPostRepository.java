072b1b74194a6c13e4d26d592c9b2b14ad5ae478
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-02 11:36:45 +0530

    feat: initiate get all job posts method

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/repositories/JobPostRepository.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/repositories/JobPostRepository.java
@@ -1,4 +1,12 @@
 package lk.job_finder_app.smart_job_aggregator.domain.repositories;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobPost;
+
+import java.util.List;
+
 public interface JobPostRepository {
+    //get all posts
+    List<JobPost> getAllJobPosts();
+
+
 }
--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobPost/JobPostUseCase.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobPost/JobPostUseCase.java
@@ -1,4 +1,10 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.jobPost;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobPost;
+
+import java.util.List;
+
 public interface JobPostUseCase {
+    //get all job posts
+    List<JobPost> getAllJobPosts();
 }
