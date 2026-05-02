ab46c7d95c1b7690d44c64bb68da08aadcf63087
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-02 11:38:24 +0530

    feat: setup get all job posts

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobPost/JobPostUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobPost/JobPostUseCaseImpl.java
@@ -1,11 +1,20 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.jobPost;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobPost;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class JobPostUseCaseImpl implements JobPostUseCase{
 
     //inject job post repo
     private final JobPostRepository jobPostRepository;
+
+    //get all job posts
+    @Override
+    public List<JobPost> getAllJobPosts(){
+        return  jobPostRepository.getAllJobPosts();
+    }
 }
