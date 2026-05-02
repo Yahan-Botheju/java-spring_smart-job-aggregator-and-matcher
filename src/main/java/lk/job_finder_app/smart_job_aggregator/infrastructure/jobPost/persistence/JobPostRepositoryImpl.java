e58adc70bb1b0c28ca37f9e0750a32f5f1eeb908
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-02 11:37:27 +0530

    feat: get all job posts from db

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/persistence/JobPostRepositoryImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/persistence/JobPostRepositoryImpl.java
@@ -1,9 +1,13 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobPost;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.entity.JobPostEntity;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.jpa.JpaJobPostRepository;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.mapper.JobPostPersistenceMapper;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class JobPostRepositoryImpl implements JobPostRepository {
@@ -13,4 +17,14 @@ public class JobPostRepositoryImpl implements JobPostRepository {
 
     //inject persistence mapper
     private final JobPostPersistenceMapper jobPostPersistenceMapper;
+
+    //get all job posts
+    @Override
+    public List<JobPost> getAllJobPosts(){
+        List<JobPostEntity> jobPosts = jpaJobPostRepository.findAll();
+
+        return jobPosts.stream().map(jobPostPersistenceMapper::toDomainModel).toList();
+    }
+
+
 }
