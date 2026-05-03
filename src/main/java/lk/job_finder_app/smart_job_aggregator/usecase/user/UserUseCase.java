e6338aff65c755acd0585b8ee9f74bb70f09c1c0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-03 22:16:54 +0530

    feat: initiate get users all method

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/user/UserUseCase.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/user/UserUseCase.java
@@ -1,4 +1,11 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.user;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.User;
+
+import java.util.List;
+
 public interface UserUseCase {
+
+    //get all users
+    List<User> getAllUsers();
 }
