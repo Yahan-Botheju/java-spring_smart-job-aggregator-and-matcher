3a7641db92a8b96a3c850f3ba7e2af4748fa2c8f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-03 22:18:28 +0530

    feat: create get all users route

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/user/controllers/UserController.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/user/controllers/UserController.java
@@ -1,10 +1,18 @@
 package lk.job_finder_app.smart_job_aggregator.web.user.controllers;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.User;
+import lk.job_finder_app.smart_job_aggregator.globalResponseHandler.StandardResponse;
 import lk.job_finder_app.smart_job_aggregator.usecase.user.UserUseCase;
+import lk.job_finder_app.smart_job_aggregator.web.user.DTOs.UserResponseDTO;
 import lk.job_finder_app.smart_job_aggregator.web.user.webMappers.UserWebMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/jobsapplicator/users/")
@@ -16,4 +24,20 @@ public class UserController {
 
     //inject user web mapper
     private final UserWebMapper userWebMapper;
+
+    //get all users
+    @GetMapping("/")
+    public ResponseEntity<StandardResponse<List<UserResponseDTO>>> getAllUsers(){
+        List<User> usersDomainModel = userUseCase.getAllUsers();
+
+        List<UserResponseDTO> toResponseDTO = usersDomainModel.stream().map(userWebMapper::toResponseDTO).toList();
+
+        return ResponseEntity.ok( new StandardResponse<>(
+                200,
+                "users data featched successfully",
+                LocalDateTime.now(),
+                toResponseDTO
+        ));
+
+    }
 }
