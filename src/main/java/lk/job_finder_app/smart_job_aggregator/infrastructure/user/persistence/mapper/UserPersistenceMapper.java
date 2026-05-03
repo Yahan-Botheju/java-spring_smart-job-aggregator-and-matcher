a06e6dec29c699024d8715d7c766a2148f650486
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-03 21:10:14 +0530

    feat: implement both persistence and web mappers with required methods

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/persistence/mapper/UserPersistenceMapper.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/persistence/mapper/UserPersistenceMapper.java
@@ -1,4 +1,25 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.mapper;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.User;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+
+@Mapper(componentModel = "spring",
+nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface UserPersistenceMapper {
+
+    //domain model to entity
+    UserEntity toEntity(User user);
+
+    //entity to domain model
+    User toDomainModel(UserEntity userEntity);
+
+    //update existing entity by new domain model
+    UserEntity updateEntity(User user, @MappingTarget UserEntity userEntity );
+
 }
