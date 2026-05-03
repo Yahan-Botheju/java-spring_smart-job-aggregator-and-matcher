9387dbc2f8c654b136cb3a60a701843b12cdc44d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-03 22:17:32 +0530

    feat: get users from db

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/persistence/UserRepositoryImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/persistence/UserRepositoryImpl.java
@@ -1,9 +1,16 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence;
 
+import aQute.bnd.annotation.headers.Category;
+import lk.job_finder_app.smart_job_aggregator.domain.models.User;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.entity.UserEntity;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.jpa.JpaUserRepository;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.mapper.UserPersistenceMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
@@ -12,4 +19,18 @@ public class UserRepositoryImpl implements UserRepository {
 
     //inject user persistence mapper
     private final UserPersistenceMapper userPersistenceMapper;
+
+
+    //get user by ID (CUSTOM METHOD FOR VALIDATION)
+    @Override
+    public Optional<User> userFindById(Long userId){
+        return jpaUserRepository.findById(userId).map(userPersistenceMapper::toDomainModel);
+    }
+
+    //get all users
+    @Override
+    public List<User> getAllUsers(){
+        List<UserEntity>  userEntities = jpaUserRepository.findAll();
+        return userEntities.stream().map(userPersistenceMapper::toDomainModel).toList();
+    }
 }
