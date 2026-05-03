4eee2e41d8bbc59cd8d623698e0fbefcbadad712
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-03 21:11:09 +0530

    feat: implement request and response templates with required fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/user/DTOs/UserRequestDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/user/DTOs/UserRequestDTO.java
@@ -1,4 +1,18 @@
 package lk.job_finder_app.smart_job_aggregator.web.user.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDTO {
+    @NotBlank(message = "Username is required")
+    private String userName;
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String userEmail;
 }
