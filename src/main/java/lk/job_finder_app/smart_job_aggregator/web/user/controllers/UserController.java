0b722856452a2f7e6ff0b22882dd6a0142b5e9ed
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-03 21:51:38 +0530

    feat: inject mapper and usecase

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/user/controllers/UserController.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/user/controllers/UserController.java
@@ -1,4 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.web.user.controllers;
 
+import lk.job_finder_app.smart_job_aggregator.usecase.user.UserUseCase;
+import lk.job_finder_app.smart_job_aggregator.web.user.webMappers.UserWebMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/jobsapplicator/users/")
+@RequiredArgsConstructor
 public class UserController {
+
+    //inject user usecase
+    private final UserUseCase userUseCase;
+
+    //inject user web mapper
+    private final UserWebMapper userWebMapper;
 }
