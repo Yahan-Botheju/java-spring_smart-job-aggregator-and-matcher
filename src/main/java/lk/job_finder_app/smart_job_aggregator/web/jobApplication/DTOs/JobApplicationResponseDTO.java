f07c497449b2790a575e350c051f170c0c31b9ea
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-04 21:50:20 +0530

    feat: add response fields

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/DTOs/JobApplicationResponseDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/DTOs/JobApplicationResponseDTO.java
@@ -1,4 +1,34 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobApplication.DTOs;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.enums.ApplicationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobApplicationResponseDTO {
+    //application
+    private Long jobApplicationId;
+    private double matchScore;
+    private ApplicationStatus applicationStatus;
+    private LocalDateTime appliedAt;
+    private String resumeUrl;
+
+    //user
+    private String userName;
+
+    //job post
+    private String postTitle;
+
+    //company
+    private String companyName;
+
+
+
+
+
 }
