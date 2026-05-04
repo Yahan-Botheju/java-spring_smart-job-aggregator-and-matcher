5f75d060cc495eefbfd9cecc5027b284cb414262
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-04 21:55:38 +0530

    feat: initiate web mapper

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/webMappers/JobApplicationWebMapper.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/webMappers/JobApplicationWebMapper.java
@@ -1,4 +1,16 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobApplication.webMappers;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
+import lk.job_finder_app.smart_job_aggregator.web.jobApplication.DTOs.JobApplicationRequestDTO;
+import lk.job_finder_app.smart_job_aggregator.web.jobApplication.DTOs.JobApplicationResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface JobApplicationWebMapper {
+
+    //domain model to response
+    JobApplicationResponseDTO toResponseDTO(JobApplication jobApplication);
+
+    //request to domain model
+    JobApplication toDomainModel(JobApplicationRequestDTO jobApplicationRequestDTO);
 }
