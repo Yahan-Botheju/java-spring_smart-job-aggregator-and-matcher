1e9950c677afccd3943012e8b0e4c8c0bbe3124f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-04 20:05:54 +0530

    feat: check user and set update user to domain repo

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/user/UserUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/user/UserUseCaseImpl.java
@@ -2,6 +2,7 @@ package lk.job_finder_app.smart_job_aggregator.usecase.user;
 
 import lk.job_finder_app.smart_job_aggregator.domain.models.User;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
+import lk.job_finder_app.smart_job_aggregator.globalExceptionHandler.superClasses.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -22,5 +23,17 @@ public class UserUseCaseImpl implements UserUseCase{
     @Override
     public User createUser(User user){
         return userRepository.createUser(user);
+    }
+
+    //update user
+    @Override
+    public User updateUser(
+            Long userId,
+            User user){
+        //check user availability
+        userRepository.userFindById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found" + " , " +  userId));
+
+        return userRepository.updateUser(userId, user);
     }
 }
