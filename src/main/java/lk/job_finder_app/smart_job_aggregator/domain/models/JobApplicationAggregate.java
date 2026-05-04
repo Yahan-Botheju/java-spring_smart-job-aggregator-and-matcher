36f337766e9f7d31c82424c415c2a00e60b4db97
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-04 21:51:40 +0530

    feat: create aggregate to get required response

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/JobApplicationAggregate.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/models/JobApplicationAggregate.java
@@ -1,4 +1,15 @@
 package lk.job_finder_app.smart_job_aggregator.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobApplicationAggregate {
+    private Company company;
+    private JobApplication jobApplication;
+    private JobPost jobPost;
+    private User user;
 }
