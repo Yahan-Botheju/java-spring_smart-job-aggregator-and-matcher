31bd003ed2d72a25531e88efe11fd9f964d70f65
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-05 21:53:45 +0530

    feat: setup get all job application route

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/controllers/JobApplicationController.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/controllers/JobApplicationController.java
@@ -1,10 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobApplication.controllers;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplicationAggregate;
+import lk.job_finder_app.smart_job_aggregator.globalResponseHandler.StandardResponse;
 import lk.job_finder_app.smart_job_aggregator.usecase.jobApplication.JobApplicationUseCase;
+import lk.job_finder_app.smart_job_aggregator.web.jobApplication.DTOs.JobApplicationResponseDTO;
 import lk.job_finder_app.smart_job_aggregator.web.jobApplication.webMappers.JobApplicationWebMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/jobsapplicator/jobapplications")
@@ -16,4 +25,21 @@ public class JobApplicationController {
 
     //inject use case
     private final JobApplicationUseCase jobApplicationUseCase;
+
+
+    //get all job applications
+    @GetMapping("/")
+    public ResponseEntity<StandardResponse<List<JobApplicationResponseDTO>>> getAllJobApplications(){
+        List<JobApplicationAggregate> jobApplications = jobApplicationUseCase.getAllJobApplications();
+
+        List<JobApplicationResponseDTO> responseDTOS = jobApplications.stream()
+                .map(jobApplicationWebMapper::aggregatorResponseDTO).toList();
+
+        return ResponseEntity.ok(new StandardResponse<>(
+                200,
+                "Details fetched successfully",
+                LocalDateTime.now(),
+                responseDTOS
+        ));
+    }
 }
