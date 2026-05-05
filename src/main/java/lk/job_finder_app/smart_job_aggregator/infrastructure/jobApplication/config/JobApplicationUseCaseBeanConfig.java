1e6d5dc0ba27c8e40d129a94886f472ef60944b8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-05 21:43:01 +0530

    feat: initiate required missing repos for config

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/config/JobApplicationUseCaseBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/config/JobApplicationUseCaseBeanConfig.java
@@ -1,6 +1,9 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.config;
 
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
 import lk.job_finder_app.smart_job_aggregator.usecase.jobApplication.JobApplicationUseCase;
 import lk.job_finder_app.smart_job_aggregator.usecase.jobApplication.JobApplicationUseCaseImpl;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +13,16 @@ import org.springframework.context.annotation.Configuration;
 class JobApplicationUseCaseBeanConfig {
     @Bean
     JobApplicationUseCase jobApplicationUseCase(
-            JobApplicationRepository jobApplicationRepository
+            JobApplicationRepository jobApplicationRepository,
+            CompanyRepository companyRepository,
+            JobPostRepository jobPostRepository,
+            UserRepository userRepository
     ) {
-        return new JobApplicationUseCaseImpl(jobApplicationRepository);
+        return new JobApplicationUseCaseImpl(
+                jobApplicationRepository,
+                companyRepository,
+                jobPostRepository,
+                userRepository
+        );
     }
 }
