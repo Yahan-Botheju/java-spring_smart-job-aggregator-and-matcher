4ff071d3f8cb2dd64ce14b07eb14ae7d91f11e35
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-05 11:45:47 +0530

    feat: initiate custom method

--- src/main/java/lk/job_finder_app/smart_job_aggregator/domain/repositories/JobApplicationRepository.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/domain/repositories/JobApplicationRepository.java
@@ -1,4 +1,10 @@
 package lk.job_finder_app.smart_job_aggregator.domain.repositories;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
+
+import java.util.Optional;
+
 public interface JobApplicationRepository {
+    //helper methods
+    Optional<JobApplication> jobApplicationFindById(long jobApplicationId);
 }
