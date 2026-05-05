52e7507301ba7fe8eeed4019980307eaba017c0a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-05 21:53:16 +0530

    feat: implement aggregator mapper with different model fields response

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/webMappers/JobApplicationWebMapper.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobApplication/webMappers/JobApplicationWebMapper.java
@@ -1,6 +1,7 @@
 package lk.job_finder_app.smart_job_aggregator.web.jobApplication.webMappers;
 
 import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplicationAggregate;
 import lk.job_finder_app.smart_job_aggregator.web.jobApplication.DTOs.JobApplicationRequestDTO;
 import lk.job_finder_app.smart_job_aggregator.web.jobApplication.DTOs.JobApplicationResponseDTO;
 import org.mapstruct.Mapper;
@@ -13,4 +14,20 @@ public interface JobApplicationWebMapper {
 
     //request to domain model
     JobApplication toDomainModel(JobApplicationRequestDTO jobApplicationRequestDTO);
+
+    //create aggregator mapper
+    default  JobApplicationResponseDTO aggregatorResponseDTO(
+            JobApplicationAggregate jobApplicationAggregate
+    ){
+        JobApplicationResponseDTO responseDTO = toResponseDTO(jobApplicationAggregate.getJobApplication());
+
+        //get company name
+        responseDTO.setCompanyName(jobApplicationAggregate.getCompany().getCompanyName());
+        //get username
+        responseDTO.setUserName(jobApplicationAggregate.getUser().getUserName());
+        //get post title
+        responseDTO.setPostTitle(jobApplicationAggregate.getJobPost().getPostTitle());
+
+        return responseDTO;
+    }
 }
