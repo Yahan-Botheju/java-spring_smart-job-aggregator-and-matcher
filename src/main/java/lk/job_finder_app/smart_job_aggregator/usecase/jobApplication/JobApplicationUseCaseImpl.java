ce28bd9000ad7259255e3403b1c41649602c32e2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-05 12:27:01 +0530

    feat: inject required repos

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobApplication/JobApplicationUseCaseImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobApplication/JobApplicationUseCaseImpl.java
@@ -1,6 +1,9 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.jobApplication;
 
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
+import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -8,4 +11,11 @@ public class JobApplicationUseCaseImpl implements JobApplicationUseCase {
 
     //inject job application repo
     private final JobApplicationRepository jobApplicationRepository;
+
+    //inject repositories (COMPANY, JOB-POST, USER)
+    private final CompanyRepository companyRepository;
+    private final JobPostRepository jobPostRepository;
+    private final UserRepository userRepository;
+
+
 }
