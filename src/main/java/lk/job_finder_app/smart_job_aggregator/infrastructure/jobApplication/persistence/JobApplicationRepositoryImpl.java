0fefba74f66b8d77bd5476a41d55764eae82966e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-05 11:47:05 +0530

    feat: set custom method functionality

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/persistence/JobApplicationRepositoryImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/persistence/JobApplicationRepositoryImpl.java
@@ -1,9 +1,13 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.globalExceptionHandler.superClasses.ResourceNotFoundException;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.jpa.JpaJobApplicationRepository;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.mapper.JobApplicationPersistenceMapper;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class JobApplicationRepositoryImpl implements JobApplicationRepository {
@@ -13,4 +17,12 @@ public class JobApplicationRepositoryImpl implements JobApplicationRepository {
 
     //inject persistence mapper
     private final JobApplicationPersistenceMapper jobApplicationPersistenceMapper;
+
+    /* HELPER METHODS */
+    @Override
+    public  Optional<JobApplication> jobApplicationFindById(long jobApplicationId){
+        return jpaJobApplicationRepository.findById(jobApplicationId)
+                .map(jobApplicationPersistenceMapper::toDomainModel);
+
+    }
 }
