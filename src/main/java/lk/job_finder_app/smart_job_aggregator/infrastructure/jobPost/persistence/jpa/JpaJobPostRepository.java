ef9cf9bd84ab9c46b4ac0d97af96b7b7d7cf9749
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-06 21:45:56 +0530

    feat: create custom query for expired job post

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/persistence/jpa/JpaJobPostRepository.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/persistence/jpa/JpaJobPostRepository.java
@@ -1,7 +1,19 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.jpa;
 
+import jakarta.transaction.Transactional;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.persistence.entity.JobPostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface JpaJobPostRepository extends JpaRepository<JobPostEntity, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE JobPostEntity j SET j.jobStatus = 'EXPIRED' " +
+            "WHERE j.jobStatus = 'ACTIVE' AND j.createdAt < :expiryLimit")
+    void expiredJobPosts(@Param("expiryLimit") LocalDate expiryLimit);
 }
