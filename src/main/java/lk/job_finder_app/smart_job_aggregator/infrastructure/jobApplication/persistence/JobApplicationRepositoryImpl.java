1fff813bbae79b795f81ac0df2f527b6c50fa1f7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-06 12:28:21 +0530

    feat: setup update job application method functionality

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/persistence/JobApplicationRepositoryImpl.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobApplication/persistence/JobApplicationRepositoryImpl.java
@@ -1,8 +1,8 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence;
 
 import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
-import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplicationAggregate;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobApplicationRepository;
+import lk.job_finder_app.smart_job_aggregator.globalExceptionHandler.superClasses.ResourceNotFoundException;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.entity.JobApplicationEntity;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.jpa.JpaJobApplicationRepository;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.jobApplication.persistence.mapper.JobApplicationPersistenceMapper;
@@ -40,5 +40,18 @@ public class JobApplicationRepositoryImpl implements JobApplicationRepository {
         JobApplicationEntity jobApplicationEntity = jobApplicationPersistenceMapper.toEntity(jobApplication);
         JobApplicationEntity savedJobApplicationEntity = jpaJobApplicationRepository.save(jobApplicationEntity);
         return jobApplicationPersistenceMapper.toDomainModel(savedJobApplicationEntity);
+    }
+
+    //update job application
+    @Override
+    public JobApplication updateJobApplication(
+            Long jobApplicationId,
+            JobApplication jobApplication
+    ){
+        JobApplicationEntity existingEntity = jpaJobApplicationRepository.findById(jobApplicationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Job Application Not Found" + " , " +  jobApplicationId));
+        JobApplicationEntity updatedEntity = jobApplicationPersistenceMapper.toUpdateEntity(jobApplication, existingEntity);
+        JobApplicationEntity savedEntity = jpaJobApplicationRepository.save(updatedEntity);
+        return jobApplicationPersistenceMapper.toDomainModel(savedEntity);
     }
 }
