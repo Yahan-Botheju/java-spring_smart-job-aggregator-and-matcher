548c9fa0300e9cf5341239dc4bc6b75c328fd7ae
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-06 11:13:52 +0530

    feat: initiate create job application method

--- src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobApplication/JobApplicationUseCase.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/usecase/jobApplication/JobApplicationUseCase.java
@@ -1,5 +1,6 @@
 package lk.job_finder_app.smart_job_aggregator.usecase.jobApplication;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplication;
 import lk.job_finder_app.smart_job_aggregator.domain.models.JobApplicationAggregate;
 
 import java.util.List;
@@ -7,4 +8,7 @@ import java.util.List;
 public interface JobApplicationUseCase {
     //get all job applications
     List<JobApplicationAggregate> getAllJobApplications();
+
+    //create job application
+    JobApplicationAggregate createJobApplication(JobApplication jobApplication);
 }
