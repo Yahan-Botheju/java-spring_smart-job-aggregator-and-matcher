d604ad7af979ec42f08c4bfe703358113d5d77e3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-06 21:48:55 +0530

    feat: initiate start corn job

--- src/main/java/lk/job_finder_app/smart_job_aggregator/SmartJobAggregatorApplication.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/SmartJobAggregatorApplication.java
@@ -2,8 +2,10 @@ package lk.job_finder_app.smart_job_aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SmartJobAggregatorApplication {
 
     public static void main(String[] args) {
