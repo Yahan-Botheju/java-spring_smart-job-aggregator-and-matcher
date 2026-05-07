9b45ff4f4bc5884f7e718ea1aad749a8917d43a6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-07 10:59:06 +0530

    feat: create custome query for get roles

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/role/persistence/jpa/JpaRoleRepository.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/role/persistence/jpa/JpaRoleRepository.java
@@ -1,7 +1,13 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.role.persistence.jpa;
 
+import lk.job_finder_app.smart_job_aggregator.domain.models.enums.RoleName;
 import lk.job_finder_app.smart_job_aggregator.infrastructure.role.persistence.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaRoleRepository extends JpaRepository<RoleEntity, Long> {
+
+    //create custom query for find role name
+    Optional<RoleEntity> findByRoleName(RoleName roleName);
 }
