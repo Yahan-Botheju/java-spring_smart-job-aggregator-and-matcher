f193541a2de6115cd2eadfbfc67eb929a26a2b64
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-07 16:13:46 +0530

    feat: change the request mapping

--- src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/controllers/JobPostController.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/web/jobPost/controllers/JobPostController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/jobsapplicator/jobpost/")
+@RequestMapping("/api/v1/job-post/")
 public class JobPostController {
 
     //inject use case
