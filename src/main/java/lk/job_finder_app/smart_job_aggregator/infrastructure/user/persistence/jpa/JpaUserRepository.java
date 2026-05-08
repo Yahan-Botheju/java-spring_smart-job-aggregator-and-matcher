9272041c86647a1a1a9050b219ef66e3e51f25d6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-08 10:38:29 +0530

    feat: create custom query for lazy loading

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/persistence/jpa/JpaUserRepository.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/user/persistence/jpa/JpaUserRepository.java
@@ -2,6 +2,14 @@ package lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.j
 
 import lk.job_finder_app.smart_job_aggregator.infrastructure.user.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
+
+    //create custom query for keep lazy loading
+    @Query("SELECT u FROM UserEntity u LEFT JOIN FETCH u.skillsRequired WHERE u.userId = :id")
+    Optional<UserEntity> findByIdWithSkills(@Param("id") Long id);
 }
