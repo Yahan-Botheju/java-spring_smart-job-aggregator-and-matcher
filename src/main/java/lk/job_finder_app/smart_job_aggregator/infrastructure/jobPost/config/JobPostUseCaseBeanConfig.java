bf7ab833d7b4276eadfbfd5fabe73c9ae02fdd44
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-10 19:00:51 +0530

    feat: initiate muse client

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/config/JobPostUseCaseBeanConfig.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/jobPost/config/JobPostUseCaseBeanConfig.java
@@ -3,6 +3,7 @@ package lk.job_finder_app.smart_job_aggregator.infrastructure.jobPost.config;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.CompanyRepository;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.JobPostRepository;
 import lk.job_finder_app.smart_job_aggregator.domain.repositories.UserRepository;
+import lk.job_finder_app.smart_job_aggregator.infrastructure.external_api.museAPI.client.TheMuseClient;
 import lk.job_finder_app.smart_job_aggregator.usecase.jobPost.JobPostUseCase;
 import lk.job_finder_app.smart_job_aggregator.usecase.jobPost.JobPostUseCaseImpl;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +15,9 @@ class JobPostUseCaseBeanConfig {
     JobPostUseCase jobPostUseCase(
             JobPostRepository jobPostRepository,
             CompanyRepository companyRepository,
-            UserRepository userRepository
+            UserRepository userRepository,
+            TheMuseClient theMuseClient
     ) {
-        return new JobPostUseCaseImpl(jobPostRepository, companyRepository, userRepository);
+        return new JobPostUseCaseImpl(jobPostRepository, companyRepository, userRepository,  theMuseClient);
     }
 }
