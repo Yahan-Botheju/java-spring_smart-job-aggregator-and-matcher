158c551beac988f14a2fc11299ee4ab53bd4d527
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-10 13:00:25 +0530

    feat: create new foilder path and add in java for external API

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/external_api/museAPI/DTOs/ExternalJobResponseDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/external_api/museAPI/DTOs/ExternalJobResponseDTO.java
@@ -1,4 +1,9 @@
 package lk.job_finder_app.smart_job_aggregator.infrastructure.external_api.museAPI.DTOs;
 
-public class ExternalJobResponseDTO {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record ExternalJobResponseDTO(
+
 }
