6c2e224289aa5035273d7d209601aa549cdeddff
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-05-10 13:04:39 +0530

    feat: initiate template

--- src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/external_api/museAPI/DTOs/ExternalJobResponseDTO.java
+++ src/main/java/lk/job_finder_app/smart_job_aggregator/infrastructure/external_api/museAPI/DTOs/ExternalJobResponseDTO.java
@@ -5,5 +5,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record ExternalJobResponseDTO(
+        @JsonProperty("results")List<TheMuseJob> jobs
+        ) {
+    public record TheMuseJob(
+            String name,
+            String content,
+            String company,
+            List<Location> locations
+    ) {}
 
+    public record Company(String name){}
+    public record Location(String name){}
 }
